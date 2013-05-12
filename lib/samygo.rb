a86fe887ebb98d4b557da37c2b0c8345a21c867e
Federico Soldani <pippo@daemon-ware.com>
2013-05-13 01:06:47 +0200

    Reformat Code

--- lib/samygo.rb
+++ lib/samygo.rb
@@ -2,28 +2,28 @@ require 'socket'
 require 'rexml/document'
 
 module SiriSamyGo
-	class RemoteControl
-		attr_accessor :url
-		attr_accessor :portRest
-		attr_accessor :portSoap
+  class RemoteControl
+    attr_accessor :url
+    attr_accessor :portRest
+    attr_accessor :portSoap
 
-		def initialize(url, portRest=2345, portSoap=52235)
-			self.url = url
-			self.portRest = portRest
-			self.portSoap = portSoap
-		end
+    def initialize(url, portRest=2345, portSoap=52235)
+      self.url = url
+      self.portRest = portRest
+      self.portSoap = portSoap
+    end
 
-		def sendKey(key)
-			sock = TCPSocket.new(url, portRest)
-			sock.puts key
-			sock.close
-			return 0
-		rescue
-			return -1
-		end
-	
-		def setVolume(volume)
-			soapMessage = %&<?xml version="1.0" encoding="UTF-8"?>
+    def sendKey(key)
+      sock = TCPSocket.new(url, portRest)
+      sock.puts key
+      sock.close
+      return 0
+    rescue
+      return -1
+    end
+
+    def setVolume(volume)
+      soapMessage = %&<?xml version="1.0" encoding="UTF-8"?>
 <s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
 <s:Body>
 <ns0:SetVolume xmlns:ns0="urn:schemas-upnp-org:service:RenderingControl:1">
@@ -34,14 +34,14 @@ module SiriSamyGo
 </s:Body>
 </s:Envelope>&
 
-			executeSoap('SetVolume', soapMessage)
-			return 0
-		rescue
-			return -1
-		end
+      executeSoap('SetVolume', soapMessage)
+      return 0
+    rescue
+      return -1
+    end
 
-		def getVolume()
-		soapMessage = %&<?xml version="1.0" encoding="UTF-8"?>
+    def getVolume()
+      soapMessage = %&<?xml version="1.0" encoding="UTF-8"?>
 <s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
 <s:Body>
 <ns0:GetVolume xmlns:ns0="urn:schemas-upnp-org:service:RenderingControl:1">
@@ -51,27 +51,27 @@ module SiriSamyGo
 </s:Body>
 </s:Envelope>&
 
-			response = executeSoap('GetVolume', soapMessage)
-			xmlDoc = REXML::Document.new response.body
-			return Integer(REXML::XPath.match(xmlDoc, "/s:Envelope/s:Body/u:GetVolumeResponse/CurrentVolume/text()")[0].value)
-		rescue
-			return -1
-		end
-		
-		private
+      response = executeSoap('GetVolume', soapMessage)
+      xmlDoc = REXML::Document.new response.body
+      return Integer(REXML::XPath.match(xmlDoc, "/s:Envelope/s:Body/u:GetVolumeResponse/CurrentVolume/text()")[0].value)
+    rescue
+      return -1
+    end
 
-		def executeSoap(action, soapMessage)
-			http = Net::HTTP.new(url, portSoap)
-			request = Net::HTTP::Post.new("/upnp/control/RenderingControl1")
-			request.add_field('POST', '/upnp/control/RenderingControl1 HTTP/1.0')
-			request.add_field('Content-Type', 'text/xml; charset="utf-8"')
-			request.add_field('SOAPACTION', '"SoapAction:urn:schemas-upnp-org:service:RenderingControl:1#'+action+'"')
-			request.add_field('Cache-Control', 'no-cache')
-			request.add_field('Host', url+':'+portSoap.to_s())
-			request.add_field('Content-length', soapMessage.length.to_s())
-			request.add_field('Connection', 'Close')
-			request.body = soapMessage
-			return http.request(request)
-		end
-	end
+    private
+
+    def executeSoap(action, soapMessage)
+      http = Net::HTTP.new(url, portSoap)
+      request = Net::HTTP::Post.new("/upnp/control/RenderingControl1")
+      request.add_field('POST', '/upnp/control/RenderingControl1 HTTP/1.0')
+      request.add_field('Content-Type', 'text/xml; charset="utf-8"')
+      request.add_field('SOAPACTION', '"SoapAction:urn:schemas-upnp-org:service:RenderingControl:1#'+action+'"')
+      request.add_field('Cache-Control', 'no-cache')
+      request.add_field('Host', url+':'+portSoap.to_s())
+      request.add_field('Content-length', soapMessage.length.to_s())
+      request.add_field('Connection', 'Close')
+      request.body = soapMessage
+      return http.request(request)
+    end
+  end
 end
--- lib/siriproxy-samygo.rb
+++ lib/siriproxy-samygo.rb
@@ -4,88 +4,88 @@ require 'pp'
 require 'samygo'
 
 class SiriProxy::Plugin::SamyGo < SiriProxy::Plugin
-	
-	def initialize(config = {})
-    	#if you have custom configuration options, process them here!
-  		@remote = SiriSamyGo::RemoteControl.new(config["url"], config["portRest"], config["portSoap"])
-  		puts "[SamyGO - Initialization] url: #{@remote.url}, portRest: #{@remote.portRest}, portSoap: #{@remote.portSoap}"
-  	end
 
-	def sendKey(key)
-    	if(@remote.sendKey(key) >= 0)
-    		say 'Fatto!'
-    	else
-   			say 'Errore!'
-		end		
-	end
+  def initialize(config = {})
+    #if you have custom configuration options, process them here!
+    @remote = SiriSamyGo::RemoteControl.new(config["url"], config["portRest"], config["portSoap"])
+    puts "[SamyGO - Initialization] url: #{@remote.url}, portRest: #{@remote.portRest}, portSoap: #{@remote.portSoap}"
+  end
 
-	listen_for /spegni tv/i do
-		sendKey('2')
-    	request_completed
-  	end
+  def sendKey(key)
+    if (@remote.sendKey(key) >= 0)
+      say 'Fatto!'
+    else
+      say 'Errore!'
+    end
+  end
 
-  	listen_for /stato tv/i do
-    	if %x(echo "pow 0000" | cec-client -d 1 -s "standby 0" RPI).include? 'power status: standby'
-			say 'TV spenta!'
-    	else
-    		say 'TV accesa!'
-    	end   
-    	request_completed
-  	end
+  listen_for /spegni tv/i do
+    sendKey('2')
+    request_completed
+  end
 
-  	listen_for /accendi tv/i do
-    	#if %x(echo "on 0" | cec-client -d 1 -s).include? 'opening a connection to the CEC adapter'
-    	if %x(echo "on 0000" | cec-client -d 1 -s "standby 0" RPI).include? 'opening a connection to the CEC adapter'
-			say 'Buona Visione!'
-    	else
-    		say 'Alzati e cammina!'
-    	end   
-    	request_completed
-  	end
+  listen_for /stato tv/i do
+    if %x(echo "pow 0000" | cec-client -d 1 -s "standby 0" RPI).include? 'power status: standby'
+      say 'TV spenta!'
+    else
+      say 'TV accesa!'
+    end
+    request_completed
+  end
 
-  	listen_for /tv muto/i do
-		sendKey('15')
-    	request_completed
-  	end
+  listen_for /accendi tv/i do
+    #if %x(echo "on 0" | cec-client -d 1 -s).include? 'opening a connection to the CEC adapter'
+    if %x(echo "on 0000" | cec-client -d 1 -s "standby 0" RPI).include? 'opening a connection to the CEC adapter'
+      say 'Buona Visione!'
+    else
+      say 'Alzati e cammina!'
+    end
+    request_completed
+  end
 
-  	listen_for /tv volume attuale/i do
-    	valumeActual = @remote.getVolume()
-    	if(valumeActual >= 0)
-			say "Volume attuale " + valumeActual.to_s()
-		else
-			say 'Errore!'
-		end   	
-    	request_completed
-  	end
+  listen_for /tv muto/i do
+    sendKey('15')
+    request_completed
+  end
 
-  	listen_for /tv volume ([+-]{0,1}[0-9]{1,2})/i do |number|
-		if number =~ /^[+-]{1}[0-9]{1,2}/i
-			valumeActual = @remote.getVolume()
-			if(valumeActual < 0)
-				say "Errore!"
-				request_completed
-				return
-			end
+  listen_for /tv volume attuale/i do
+    valumeActual = @remote.getVolume()
+    if (valumeActual >= 0)
+      say "Volume attuale " + valumeActual.to_s()
+    else
+      say 'Errore!'
+    end
+    request_completed
+  end
 
-			number = (valumeActual + Integer(number)).to_s()
-		end
+  listen_for /tv volume ([+-]{0,1}[0-9]{1,2})/i do |number|
+    if number =~ /^[+-]{1}[0-9]{1,2}/i
+      valumeActual = @remote.getVolume()
+      if (valumeActual < 0)
+        say "Errore!"
+        request_completed
+        return
+      end
 
-		if(@remote.setVolume(number) >= 0)
-    		say "Fatto! Volume #{number}"
-    	else
-   			say "Errore!"
-    	end
-    
-    	request_completed
-  	end
+      number = (valumeActual + Integer(number)).to_s()
+    end
 
-  	listen_for /passa a Apple TV/i do
-		sendKey('190')
-    	request_completed
-  	end
+    if (@remote.setVolume(number) >= 0)
+      say "Fatto! Volume #{number}"
+    else
+      say "Errore!"
+    end
 
- 	listen_for /passa a sky/i do
-		sendKey('233')
-    	request_completed
-  	end
+    request_completed
+  end
+
+  listen_for /passa a Apple TV/i do
+    sendKey('190')
+    request_completed
+  end
+
+  listen_for /passa a sky/i do
+    sendKey('233')
+    request_completed
+  end
 end
